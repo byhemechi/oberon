0489adf60fcae1f1d5361fb07f482d51d2575b30
George Fischer <me@george.id.au>
2025-04-19 13:19:38 +1000

    add image dep

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Oberon.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:image, "~> 0.54.4"}
     ]
   end
 
